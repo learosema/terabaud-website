5a269e5018f7bee8c10476b3b5f9989129ed8f5b
Lea Rosema <terabaud@gmail.com>
2021-01-13 13:21:16 +0100

    feat: add gh-pages deployment

---
+++ README.md
@@ -0,0 +1,8 @@
+# Eleventy website project
+
+This is a work in progress project, relaunching my website: terabaud.de
+
+```sh
+npm install
+npm start
+```
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: 'terabaud.de',
   slogan: "Welcome to Lea's World",
-  url: '',
+  url: process.env.ELEVENTY_ENV === 'production' ? 'https://terabaud.github.io/terabaud-website' : '',
 };
